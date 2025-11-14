7a201c6594710ea3475ff3852656068e9554eba3
Sadvithi08 <5668sadvithi@gmail.com>
2025-11-14 11:51:13 +0530

    Add files via upload

---
+++ odd and even numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("ener the a value");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("even is %d",a);
+	}
+	else
+	{
+		printf("odd is %d",a);
+	}
+}
